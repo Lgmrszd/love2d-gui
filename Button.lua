afb824dcece439f259a09378946aba67044f6ac5
Mikhail <m.v.bobrov@yandex.ru>
2018-10-26 01:59:55 +0300

    buttons hover and click implementation

--- Button.lua
+++ Button.lua
@@ -1,5 +1,12 @@
 gui = require("gui")
+
+local border = 5
+
 local Button = {}
+local color = {81/255, 81/255, 81/255, 1}
+local hovered_color = {120/255, 120/255, 120/255, 1}
+local pressed_color = {170/255, 170/255, 170/255, 1}
+local text_color = {220/255, 220/255, 220/255, 1}
 Button.__index = Button
 
 setmetatable(Button, {
@@ -14,16 +21,53 @@ setmetatable(Button, {
 function Button:_init()
   gui.Widget._init(self)
   self.text = "Button"
-  self.w = 100
-  self.h = 40
-  self.color = {200/255, 200/255, 30/255, 1}
+  self.w = assets.fonts.buttons_font:getWidth(self.text) + border*2
+  self.h = assets.fonts.buttons_font:getHeight() + border*2
+end
+
+function Button:get_minimum_size()
+  local min_w = assets.fonts.buttons_font:getWidth(self.text) + border*2
+  local min_h = assets.fonts.buttons_font:getHeight() + border*2
+  return min_w, min_h
+end
+
+function Button:resize(w, h)
+  min_w, min_h = self:get_minimum_size()
+  self.w = math.max(w, min_w)
+  self.h = math.max(h, min_h)
+  return self.w, self.h
+end
+
+function Button:move(x, y)
+  self.x = x
+  self.y = y
+end
+
+function Button:set_text(text)
+  self.text = text
+  self:resize(self.w, self.h)
+end
+
+function Button:has_clicked(x, y)
+  self:set_text("I has been clicked!")
 end
 
 function Button:draw()
   local r, g, b, a = love.graphics.getColor()
-  love.graphics.setColor(self.color)
+  local previous_font = love.graphics.getFont()
+  if self.is_pressed then
+    love.graphics.setColor(pressed_color)
+  elseif self.is_hovered then
+    love.graphics.setColor(hovered_color)
+  else
+    love.graphics.setColor(color)
+  end
+  love.graphics.setFont(assets.fonts.buttons_font)
   love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+  love.graphics.setColor(text_color)
+  love.graphics.printf(self.text, self.x+border, self.y+border, self.w-border*2, "center")
   love.graphics.setColor(r, g, b, a)
+  love.graphics.setFont(previous_font)
 end
 
 return Button
--- init.lua
+++ init.lua
@@ -1,12 +1,10 @@
---Button = require
+assets = require("assets")
 
 local gui = {}
--- local widgets = {}
-widgets = {}
+local widgets = {}
 
 local Widget = {}
 Widget.__index = Widget
-
 setmetatable(Widget, {
   __call = function()
       obj = setmetatable({}, Widget)
@@ -14,25 +12,59 @@ setmetatable(Widget, {
       return obj
   end
 })
-
-love.graphics.setNewFont(40)
-font = love.graphics.getFont()
--- font:setDPIScale(2)
-
 function Widget:_init()
   self.x = 0
   self.y = 0
   self.w = 0
   self.h = 0
+  self.is_hovered = false
+  self.is_pressed = false
   table.insert(widgets, self)
+end
+
+function Widget:contains(x, y)
+  return x > self.x and y > self.y and
+  x < (self.x + self.w) and y < (self.y + self.h)
+end
+
+function Widget:has_clicked(x, y)
 end
 
 function gui.draw()
   for _, widget in pairs(widgets) do
     widget:draw()
   end
-  love.graphics.print("gtest, \nh: "..font:getHeight(), 100, 100)
+end
 
+function gui.mousemoved(x, y, dx, dy)
+  for _, widget in pairs(widgets) do
+    if widget:contains(x, y) then
+      widget.is_hovered = true
+    else
+      widget.is_hovered = false
+    end
+  end
+end
+
+function gui.mousepressed(x, y, dx, dy)
+  for _, widget in pairs(widgets) do
+    if widget:contains(x, y) then
+      widget.is_pressed = true
+    else
+      widget.is_pressed = false
+    end
+  end
+end
+
+function gui.mousereleased(x, y, dx, dy)
+  for _, widget in pairs(widgets) do
+    if widget.is_pressed then
+      if widget:contains(x, y) then
+        widget:has_clicked(x, y)
+      end
+    end
+    widget.is_pressed = false
+  end
 end
 
 --gui.Button = Button
